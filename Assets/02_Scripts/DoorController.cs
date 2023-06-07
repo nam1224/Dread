9ad83bb221edc5fbc7fb1e3c990fb147177eb723
junhyuk310 <ujunhyuk310@gmail.com>
2023-06-07 22:54:39 +0900

    plus

--- Assets/02_Scripts/DoorController.cs
+++ Assets/02_Scripts/DoorController.cs
@@ -3,26 +3,28 @@ using System.Collections;
 
 public class DoorController : MonoBehaviour
 {
+    public int OpenDgree = -90;
     public float openSpeed = 2f; // 문이 열리는 속도
 
     private bool isOpen = false; // 문이 열려있는지 여부를 추적하기 위한 변수
+    private bool isDoorAnimating = false; // 문 애니메이션이 진행 중인지 여부를 추적하기 위한 변수
     private Quaternion closedRotation; // 닫힌 상태의 회전값
     private Quaternion openedRotation; // 열린 상태의 회전값
-
+    
     private bool isPlayerNearby = false; // 플레이어가 문 주변에 있는지 여부
 
     private void Start()
     {
         // 닫힌 상태와 열린 상태의 회전값 계산
         closedRotation = transform.rotation;
-        openedRotation = Quaternion.Euler(0, 90, 0);
+        openedRotation = Quaternion.Euler(0, OpenDgree, 0);
     }
 
     private void Update()
     {
-        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E) && !isDoorAnimating)
         {
-            // 플레이어가 문 주변에서 E 키를 누를 때 문을 열거나 닫기
+            // 플레이어가 문 주변에서 E 키를 누르고 문 애니메이션이 진행 중이 아닐 때만 문을 열거나 닫기
             if (isOpen)
             {
                 CloseDoor();
@@ -50,6 +52,7 @@ public class DoorController : MonoBehaviour
 
     private IEnumerator OpenAnimation()
     {
+        isDoorAnimating = true;
         float t = 0f;
         while (t < 1f)
         {
@@ -58,10 +61,12 @@ public class DoorController : MonoBehaviour
             yield return null;
         }
         isOpen = true;
+        isDoorAnimating = false;
     }
 
     private IEnumerator CloseAnimation()
     {
+        isDoorAnimating = true;
         float t = 0f;
         while (t < 1f)
         {
@@ -70,6 +75,7 @@ public class DoorController : MonoBehaviour
             yield return null;
         }
         isOpen = false;
+        isDoorAnimating = false;
     }
 
     private void OnTriggerEnter(Collider other)
