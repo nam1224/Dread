59a41e9437756d62371dd392c448d02193246b51
nam1224 <105091319+nam1224@users.noreply.github.com>
2023-06-22 14:51:44 +0900

    fix_InteractionRaycast
    레이케스트 상호작용 문제를 해결함
    원인 : 레이케스트 방향이 잘못 조정되어 있었음

--- Assets/02_Scripts/Interaction.cs
+++ Assets/02_Scripts/Interaction.cs
@@ -7,12 +7,6 @@ public class Interaction : MonoBehaviour
     public float triggerRadius = 5f; //플레이어가 이동하면서 상호작용 가능한 물체르 탐지할 범위
     public LayerMask targetLayer; //플레이어가 탐지할 레이어는 Item
 
-    Vector3 screenCenter;
-    private void Start()
-    {
-        screenCenter = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2);
-    }
-
     //Flashlight중 chargeEnergy를 사용하기 위함
     public Flashlight flashlight;
     public TextManager textmanager;
@@ -20,36 +14,15 @@ public class Interaction : MonoBehaviour
     //퍼즐을 작동하기 위함
     public LeverPuzzle leverPuzzle;
     public SafePuzzleManager SafePuzzleManager;
-    //
     public GameObject filmLight;
     //Raycast와 같이 사용할 친구들~
-    public Camera cam;
-    RaycastHit hit;
 
-    int layerMask = 1 << 8; //8번만 true
     private void Update()
     {
-        DetectObjectRaycase();
         bool detection = DetectObject();
         OnInteractionEnter(detection);
         OnInteractionStay(detection, itemInRange, colliders);
         OnInteractionExit(detection);
-    }
-
-    private void DetectObjectRaycase()
-    {
-        layerMask = ~layerMask;
-        Ray ray = cam.ScreenPointToRay(screenCenter);
-        //Debug.DrawRay(transform.position, transform.forward * triggerRadius, Color.red);
-        //Physics.Raycast(ray 원점 위치, 레이저 쏠 방향, 충돌 감지 hit)
-        //Raycast에 오브젝트가 감지되고 마우스가 클릭됐다면
-        if (Physics.Raycast(transform.position, transform.forward, out hit, layerMask) 
-            && Input.GetMouseButtonDown(0))
-        {
-            //감지할 오브젝트와 관련된 함수
-            leverPuzzle.PullLever(hit);
-            Debug.Log(hit.collider.name);
-        }
     }
 
 
---
+++ Assets/02_Scripts/RaycastInteraction.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastInteraction : MonoBehaviour
+{
+    public Camera cam;
+    RaycastHit hit;
+    Vector3 screenCenter;
+    public float triggerRadius;
+
+    public LeverPuzzle leverPuzzle;
+
+    int layerMask = 1 << 8;
+    private void Start()
+    {
+        screenCenter = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2);
+    }
+
+    private void Update()
+    {
+        DetectObjectRaycast();
+    }
+
+    private void DetectObjectRaycast()
+    {
+        layerMask = ~layerMask;
+        Ray ray = cam.ScreenPointToRay(screenCenter);
+        Debug.DrawRay(transform.position, transform.forward * triggerRadius, Color.red);
+        //Physics.Raycast(ray 원점 위치, 레이저 쏠 방향, 충돌 감지 hit)
+        //Raycast에 오브젝트가 감지되고 마우스가 클릭됐다면
+        if (Physics.Raycast(transform.position, transform.forward, out hit, layerMask)
+            && Input.GetMouseButtonDown(0))
+        {
+            //감지할 오브젝트와 관련된 함수
+            leverPuzzle.PullLever(hit);
+            Debug.Log(hit.collider.name);
+        }
+    }
+
+}
